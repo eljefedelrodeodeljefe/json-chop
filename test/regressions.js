a4878bfa240b576da5237ce4a13eb07aa3778797
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-05-14 17:34:00 +0200

    no arrow function in test

--- test/regressions.js
+++ test/regressions.js
@@ -1,7 +1,7 @@
 'use strict'
 var test = require('tape')
 
-test('Regression [1] options not available', (t) => {
+test('Regression [1] options not available', function (t) {
   t.plan(2)
   var chop = require('../')
   var obj = {
