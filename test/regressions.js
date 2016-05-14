f66f80447fa8e80fee97aa3724003580cce3ba64
Robert Jefe Lindstädt <robert.lindstaedt@gmail.com>
2016-05-14 17:37:12 +0200

    add regression tests; fix missing options; refine readme (#1)
    * standard as local dep
    
    * adds regressions or arbitrary tests
    
    * catch missing option keys
    
    * refine readme
    
    * no arrow function in test
    
    * have arrow function in example

---
+++ test/regressions.js
@@ -0,0 +1,29 @@
+'use strict'
+var test = require('tape')
+
+test('Regression [1] options not available', function (t) {
+  t.plan(2)
+  var chop = require('../')
+  var obj = {
+    somekey: {
+      nestedKey: 'hello',
+      _id: 12234555,
+      version: null,
+      __v: '0.8.0',
+      oneHunrdred: 100
+    }
+  }
+
+  var options_1 = {
+    valueBlacklist: ['', 100, null] // empty arrays and undefied are chopped of implicitly
+  }
+
+  var res = chop.chop(obj, options_1)
+  t.deepEquals(res, JSON.stringify({somekey: {nestedKey: 'hello',_id: 12234555,  __v: '0.8.0'}}))
+
+  var options_2 = {
+    keyBlacklist: ['__v', '_id'],
+  }
+  var res = chop.chop(obj, options_2)
+  t.deepEquals(res, JSON.stringify({somekey: {nestedKey: 'hello',version: null,  oneHunrdred: 100}}))
+})
