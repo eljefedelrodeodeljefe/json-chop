446e1662c48816f0da714bbcd6b770d3451bb919
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-05-14 17:24:23 +0200

    catch missing option keys

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 exports.chop = function chop (obj, options, cb) {
   var res = JSON.stringify(obj, function chopFilter (key, value) {
-    if (options.keyBlacklist.indexOf(key) > -1) {
+    if (options.keyBlacklist && // bail if not present
+        options.keyBlacklist.indexOf(key) > -1) {
       return undefined
     }
 
-    if (options.valueBlacklist.indexOf(value) > -1 || value === undefined || value.length === 0) {
+    if (options.valueBlacklist && // bail if not present
+        (options.valueBlacklist.indexOf(value) > -1 ||
+         value === undefined ||
+         value.length === 0)) {
       return undefined
     }
     return value
